drakkar-lig/socklab

commit 6e6e0eb7e9e228118c49cf26d46343a74b434134

Makefile: fix build on ubuntu 16.04
The compiler on Ubuntu 16.04, GCC 5.4.0, is sensitive to ordering of its
arguments.  We were passing the libs first and then the .o objects
afterwards, and it didn't like it.

Before (fails with old GCC):

    cc  -lreadline -lhistory  socklab.o prim.o options.o tools.o tcp.o udp.o version.o   -o socklab

After (works with old GCC):

    cc   socklab.o prim.o options.o tools.o tcp.o udp.o version.o  -lreadline -lhistory  -o socklab

Makefile
@@ -38,7 +38,8 @@ TARGETS=socklab
 OBJS	= $(SOURCES:.c=.o)
 
 # Let packagers define LDFLAGS and CFLAGS if they want to, but add our own flags
-LDFLAGS := -lreadline -lhistory $(LDFLAGS)
+LDFLAGS := $(LDFLAGS)
+LDLIBS  := -lreadline -lhistory 
 CFLAGS  := -Wall $(CFLAGS)
 
 # Adaptation a Darwin / MacOS X

commit 08d0ef3b912569bab98f9a7a0307c06a1ef0f00a

Fix build on OS X
-lhistory does not exist on OS X.  This is likely a regression caused by
6e6e0eb7e9e2281.

Makefile
@@ -39,7 +39,7 @@ OBJS	= $(SOURCES:.c=.o)
 
 # Let packagers define LDFLAGS and CFLAGS if they want to, but add our own flags
 LDFLAGS := $(LDFLAGS)
-LDLIBS  := -lreadline -lhistory 
+LDLIBS  := -lreadline -lhistory
 CFLAGS  := -Wall $(CFLAGS)
 
 # Adaptation a Darwin / MacOS X
@@ -52,7 +52,7 @@ else
 # OSX readline does not support invisible characters in prompt
 # http://stackoverflow.com/questions/31329952
 CPPFLAGS+=-DOSX_READLINE
-LDFLAGS	= -lreadline
+LDLIBS	= -lreadline
 endif
 endif
 